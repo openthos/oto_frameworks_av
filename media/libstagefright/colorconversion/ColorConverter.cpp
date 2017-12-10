android-x86-mirror/frameworks-av

media: enable dithering for RGB565 conversion
This seems to improve video playback quality.

Inspired by a patch from WuZhen.

media/libstagefright/colorconversion/ColorConverter.cpp
@@ -226,8 +226,8 @@ status_t ColorConverter::convertYUV420PlanarUseLibYUV(
         src_u + (src.mWidth / 2) * (src.mHeight / 2);
 
 
-    libyuv::I420ToRGB565(src_y, src.mWidth, src_u, src.mWidth / 2, src_v, src.mWidth / 2,
-            (uint8 *)dst_ptr, dst.mWidth * 2, dst.mWidth, dst.mHeight);
+    libyuv::I420ToRGB565Dither(src_y, src.mWidth, src_u, src.mWidth / 2, src_v, src.mWidth / 2,
+            (uint8 *)dst_ptr, dst.mWidth * 2, NULL, dst.mWidth, dst.mHeight);
 
     return OK;
 }

openthos/oto_frameworks_av

media/libstagefright/colorconversion/ColorConverter.cpp
@@ -253,8 +253,8 @@ status_t ColorConverter::convertYUV420PlanarUseLibYUV(
 
     switch (mDstFormat) {
     case OMX_COLOR_Format16bitRGB565:
-        libyuv::I420ToRGB565(src_y, src.mStride, src_u, src.mStride / 2, src_v, src.mStride / 2,
-                (uint8 *)dst_ptr, dst.mStride, src.cropWidth(), src.cropHeight());
+        libyuv::I420ToRGB565Dither(src_y, src.mStride, src_u, src.mStride / 2, src_v, src.mStride / 2,
+                (uint8 *)dst_ptr, dst.mStride, NULL, src.cropWidth(), src.cropHeight());
         break;
 
     case OMX_COLOR_Format32BitRGBA8888: